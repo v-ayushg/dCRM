455f68ef8599f1451abd582dc10352ccec0f63de
Ayush <29086241+gupta07ayush@users.noreply.github.com>
2024-08-04 20:45:58 +0530

    add app and mysql db in settings.py

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k4o$z*qjb2*2m+nz^6e^5un6j*7r*(f6z5rfdg48bh94--l69d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'elderco',
+        'USER':'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
