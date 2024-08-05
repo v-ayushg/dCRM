a39d70da93aaec137cfb5f5cd4f938f4cd38d98c
Ayush <29086241+gupta07ayush@users.noreply.github.com>
2024-08-05 22:16:06 +0530

    add login details in base, home and views page

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    
+    #check to see if loggin in
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-    return render(request, 'home.html', {})
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"You have been logged in")
+            return redirect('home')
+        else:
+            messages.error(request, 'There was an error login in, try again')
+            return render(request, 'home.html', {})  # Ensure response is returned
+    else:
+        return render(request, 'home.html', {})
 
 # def login_user(request):
 #     pass
