ab1ab72e1bdeca1aee2a82c8df2c5b6462eed6d1
Ayush <29086241+gupta07ayush@users.noreply.github.com>
2024-09-01 01:38:35 +0530

    add Record model and add in admin

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    v_dash = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return("f{self.first_name} {self.last_name}")
